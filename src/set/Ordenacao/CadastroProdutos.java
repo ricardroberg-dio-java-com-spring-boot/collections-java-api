1e5d890fbb938b984aaa0c86ad564eae6b5cf680
Ricard Roberg <ricardroberg@gmail.com>
2024-11-12 19:39:49 -0300

    Resolução desafio CadastroProdutos

---
+++ src/set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,43 @@
+package set.Ordenacao;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+
+    private Set<Produto> produtosSet;
+
+    public CadastroProdutos() {
+        this.produtosSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long codigo, String nome, double preco, int quantidade) {
+        produtosSet.add(new Produto(codigo, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        Set<Produto> produtosPorNome = new TreeSet<>(produtosSet);
+        return produtosPorNome;
+    }
+
+    public Set<Produto> exibirProdutosPorPreco() {
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtosPorPreco.addAll(produtosSet);
+        return produtosPorPreco;
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+        cadastroProdutos.adicionarProduto(1L, "Produto 5", 15d, 5);
+        cadastroProdutos.adicionarProduto(2L, "Produto 0", 20d, 10);
+        cadastroProdutos.adicionarProduto(1L, "Produto 3", 10d, 2);
+        cadastroProdutos.adicionarProduto(9L, "Produto 9", 2d, 2);
+
+        System.out.println(cadastroProdutos.produtosSet);
+
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
+}
